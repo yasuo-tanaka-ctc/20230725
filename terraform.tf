ceabd98003540e8dd4ba20e3635137e7d2e8b722
Yasuo Tanaka <yasuo.tanaka.924@ctc-g.co.jp>
2023-07-25 17:27:16 +0900

    Remove cloud block

--- terraform.tf
+++ terraform.tf
@@ -1,13 +1,13 @@
 terraform {
-
+/*
   cloud {
-    organization = "organization-name"
+    organization = "20230725"
 
     workspaces {
       name = "learn-terraform-cloud"
     }
   }
-
+*/
   required_providers {
     aws = {
       source  = "hashicorp/aws"
